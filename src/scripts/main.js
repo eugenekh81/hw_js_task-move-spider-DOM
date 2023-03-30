212a2b2b67fa5581b8f069d0efc8152dbf3d526f
Alex <kartanovaleks@gmail.com>
2023-03-30 12:31:51 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -10,10 +10,21 @@ document.addEventListener("click", (e) => {
     wall.clientLeft -
     spider.offsetWidth / 2 +
     "px";
+
   spider.style.top =
     e.clientY -
     wall.offsetTop -
     wall.clientTop -
     spider.offsetHeight / 2 +
     "px";
+
+  if (
+    e.clientX > wall.getBoundingClientRect().right ||
+    e.clientX < wall.getBoundingClientRect().left ||
+    e.clientY > wall.getBoundingClientRect().bottom ||
+    e.clientY < wall.getBoundingClientRect().top
+  ) {
+    spider.style.left = 0;
+    spider.style.top = 0;
+  }
 });
