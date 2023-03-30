e87859ea129f3dfa6a587c8f19da95ca4e66b358
Alex <kartanovaleks@gmail.com>
2023-03-30 12:10:49 +0300

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,19 @@
-'use strict';
+"use strict";
 
-document.addEventListener('click', e => {
-  // write code here
+const spider = document.querySelector(".spider");
+const wall = document.querySelector(".wall");
+
+document.addEventListener("click", (e) => {
+  spider.style.left =
+    e.clientX -
+    wall.offsetLeft -
+    wall.clientLeft -
+    spider.offsetWidth / 2 +
+    "px";
+  spider.style.top =
+    e.clientY -
+    wall.offsetTop -
+    wall.clientTop -
+    spider.offsetHeight / 2 +
+    "px";
 });
